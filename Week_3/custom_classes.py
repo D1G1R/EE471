a4f3abb0fed3d3834399e76d20e40fe059e70810
Ugurcan <ugurcantomruk2004@hotmail.com>
2026-03-12 16:12:28 +0300

    Week 3 task completed

--- Week_3/custom_classes.py
+++ Week_3/custom_classes.py
@@ -5,25 +5,26 @@ class MediaItem(object):
         self.is_available = is_available
 
     def checkout(self):
-        if self.is_available == True:
-            self.is_available = False
+        if self.is_available == True: # check if is.available is True or False
+            self.is_available = False # if True than set it to False
             return("Succesful checkout.")
         else:
-            return("Already out, sorry.")
+            return("Already out, sorry.") 
         
     def return_item(self):
         self.is_available = True
 
     def __str__(self):
-        if self.is_available == True:
+        if self.is_available == True: # A temporary variable 
             temp = "Available"
         else:
             temp = "Checked Out"
         return str(self.title) + " by " + str(self.author) + " [" + temp + "]"
     
+# --- Part 2 --- 
 class Book(MediaItem):
     def __init__(self, title, author, isavailable, page_count):
-        super().__init__(title, author, isavailable)
+        super().__init__(title, author, isavailable) # super used to gather features from MediaItem
         self.page_count = page_count
 
     def __str__(self):
@@ -40,9 +41,13 @@ class DVD(MediaItem):
     def checkout(self):
         return super().checkout() + " Handle with care: Do not scratch the disc."
     
+#Question: What is the use of super() here? What happens if we change the logic MediaItem.Checkout() later?
+#Ans: Super() calls the parent class's method so we can reuse and extend its logic without rewriting code. 
+# If we update MediaItem.checkout(), the changes automatically apply to all subclasses. This ensures consistency and makes the code much easier to maintain.
+# --- Part 3 ---
 class LibraryCollection(object):
     def __init__(self):
-        self.items = []
+        self.items = [] # An empty list created so we can add items later
 
     def add_item(self, new_item):
         self.items.append(new_item)
@@ -55,7 +60,7 @@ class LibraryCollection(object):
                 available_titles.append(item.title)
 
         return available_titles
-        #return [item.title for item in self.items if item.is_available]
+        # "return [item.title for item in self.items if item.is_available]" this is the shorter way
 
 
 
--- Week_3/main.py
+++ Week_3/main.py
@@ -4,31 +4,31 @@ from custom_classes import MediaItem, Book, DVD, LibraryCollection
 book_1 = MediaItem("Kumarbaz","Dostoyevski",True)
 movie_1 = MediaItem("Ahlat Ağacı","Nuri Bilge Ceylan",False)
 
-print(book_1.checkout())
-print(book_1.checkout())
-print(movie_1.checkout())
+print(book_1.checkout()) # Book 1 checked out
+print(book_1.checkout()) # Book 1 checked out again but not available
+print(movie_1.checkout()) # Movie 1 checked out but not available
 
 # --- Part 2 ---
-book_2 = Book("Korluk", "Jose Saramago", True, 265)
-movie_2 = DVD("Martian", "Unknown", True, 173)
+book_2 = Book("Korluk", "Jose Saramago", True, 265) # New book and movie added with subclasses
+movie_2 = DVD("Martian", "Unknown", True, 173) 
 
-print(book_2)
-print(book_2.checkout())
-print(movie_2)
-print(movie_2.checkout())
+print(book_2) # View the info about book_2
+print(book_2.checkout()) # Book_2 checked out
+print(movie_2) # View the info about movie_2
+print(movie_2.checkout()) # Movie_2 checked out
 
 # --- Part 3 ---
-library = LibraryCollection()
+library = LibraryCollection() # Library created
 
-library.add_item(book_1)
+library.add_item(book_1) # New items added to the library
 library.add_item(book_2)
 library.add_item(movie_1)
 library.add_item(movie_2)
 
-print(library.list_available())
+print(library.list_available()) # This gave us an empty list because we already checked out items
 
-book_1.return_item()
+book_1.return_item() # Returned items
 book_2.return_item()
 movie_2.return_item()
 
-print(library.list_available())
+print(library.list_available()) # Now we can view the available items in our library
